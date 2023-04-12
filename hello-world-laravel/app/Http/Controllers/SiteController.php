85a4aa68c668d8dd6c804e32993b3d569094ab18
Charles Rocha <rocha_charles@outlook.com>
2023-04-12 18:38:45 -0300

    exemplo laravel

---
+++ hello-world-laravel/app/Http/Controllers/SiteController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    //
+    public function index() {
+        $nome = "Charles";
+
+        $data = [
+            'apelido_nome' => $nome,
+        ];
+
+       return view('bemvindo', $data);
+    }
+
+    public function exit() {
+        return view("sair");
+    }
+
+
+    public function users(Request $r){
+
+        $data = [
+            'quantidade' => $r->qnt
+        ];
+        //http://localhost:8000/usuarios/3
+        return view("usuarios", $data);
+    }
+}
